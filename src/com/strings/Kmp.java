ec504f61fb9ca032cc2592174123c23aa282d52e
Second Chen <second_chen@trendmicro.com>
2021-08-24 22:20:58 +0800

    add spin word example

--- src/com/strings/Kmp.java
+++ src/com/strings/Kmp.java
@@ -8,13 +8,21 @@ import java.util.Arrays;
  */
 public class Kmp {
 
-  public static void main(String[] args) {
-    String a = "ABACABAD";
-    String b = "BBC ABACABACABAD ABCDABDE";
-    int result = kmp(b, a);
+  /** 计算部分匹配表 */
+  public static int[] kmpnext(String dest) {
+    int[] next = new int[dest.length()];
+    next[0] = 0;
 
-    // 打印结果：和字符串获得匹配的位置
-    System.out.println("resultPosion:" + result);
+    for (int i = 1, j = 0; i < dest.length(); i++) {
+      while (j > 0 && dest.charAt(j) != dest.charAt(i)) {
+        j = next[j - 1];
+      }
+      if (dest.charAt(i) == dest.charAt(j)) {
+        j++;
+      }
+      next[i] = j;
+    }
+    return next;
   }
 
   /** KMP 匹配 */
@@ -38,20 +46,29 @@ public class Kmp {
     return -1;
   }
 
-  /** 计算部分匹配表 */
-  public static int[] kmpnext(String dest) {
-    int[] next = new int[dest.length()];
-    next[0] = 0;
+  /*
+   * 判断str1和str2是否互为旋转词: str1可以通过str2旋转获得，str2也可以通过str1旋转获得
+   * str1 = "abcd"
+   * str2 = "cdab"
+   * str1与str2互为旋转词
+   */
+  public static boolean isSpin(String str1, String str2) {
+    String tmp = str1 + str1;
+    int index = kmp(tmp, str2);
+    return index != -1;
+  }
 
-    for (int i = 1, j = 0; i < dest.length(); i++) {
-      while (j > 0 && dest.charAt(j) != dest.charAt(i)) {
-        j = next[j - 1];
-      }
-      if (dest.charAt(i) == dest.charAt(j)) {
-        j++;
-      }
-      next[i] = j;
-    }
-    return next;
+  public static void main(String[] args) {
+    String a = "ABACABAD";
+    String b = "BBC ABACABACABAD ABCDABDE";
+    int result = kmp(b, a);
+
+    // 打印结果：和字符串获得匹配的位置
+    System.out.println("result position: " + result);
+
+    String str1 = "abcdefg";
+    //    String str2 = "defgabc";
+    String str2 = "defgacc";
+    System.out.println("str1和str互为旋转词: " + isSpin(str1, str2));
   }
 }
