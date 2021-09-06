9e9a9dfccdbe0d2d115be83e4b5d4fadee18ab0e
Second Chen <second_chen@trendmicro.com>
2021-09-06 19:48:54 +0800

    hash table and hash func

---
+++ src/com/hashtable/RandomPool.java
@@ -0,0 +1,89 @@
+package com.hashtable;
+
+import java.util.HashMap;
+
+/*
+ * final && static
+ * final
+ * 对于一个final变量，如果是基本数据类型的变量，则其数值一旦在初始化之后便不能更改；
+ * 如果是引用类型的变量，则在对其初始化之后便不能再让其指向另一个对象。
+ *
+ * static
+ * 方便在没有创建对象的情况下来进行调用（方法/变量）。
+ * 很显然，被static关键字修饰的方法或者变量不需要依赖于对象来进行访问，只要类被加载了，就可以通过类名去进行访问。
+ */
+
+/*
+ * 设计RandomPool结构
+ *
+ * 设计一种结构，在该结构上有如下三种功能:
+ * insert(key): 将某个key加入到该结构，做到不重复加入
+ * delete(key): 将原本在结构中的某个key移除
+ * getRandom(): 等概率随机返回结构中的任何一个key
+ *
+ * [要求]
+ * Insert, delete和getRandom方法的时间复杂度都是O(1)
+ *
+ * refer: images/pool.png
+ */
+
+public class RandomPool<K> {
+  private final HashMap<K, Integer> keyIndexMap;
+  private final HashMap<Integer, K> indexKeyMap;
+
+  private int size;
+
+  public RandomPool() {
+    this.keyIndexMap = new HashMap<K, Integer>();
+    this.indexKeyMap = new HashMap<Integer, K>();
+    this.size = 0;
+  }
+
+  public void insert(K key) {
+    // 不重复插入
+    if (!this.keyIndexMap.containsKey(key)) {
+      this.keyIndexMap.put(key, this.size);
+      this.indexKeyMap.put(this.size++, key);
+    }
+  }
+
+  public void delete(K key) {
+    if (this.keyIndexMap.containsKey(key)) {
+      int deleteIndex = this.keyIndexMap.get(key);
+      int lastIndex = --this.size;
+      K lastKey = this.indexKeyMap.get(lastIndex);
+      this.keyIndexMap.put(lastKey, deleteIndex);
+      this.indexKeyMap.put(deleteIndex, lastKey);
+      this.keyIndexMap.remove(key);
+      this.indexKeyMap.remove(lastIndex);
+    }
+  }
+
+  public K getRandom() {
+    if (this.size == 0) {
+      return null;
+    }
+    int randomIndex = (int) (Math.random() * this.size);
+    return this.indexKeyMap.get(randomIndex);
+  }
+
+  public String toString() {
+    return "pool: " + this.indexKeyMap + ", pool size: " + this.size;
+  }
+
+  public static void main(String[] args) {
+    RandomPool<String> pool = new RandomPool<>();
+    pool.insert("a");
+    pool.insert("b");
+    pool.insert("b");
+    pool.insert("v");
+
+    System.out.println(pool);
+
+    String str = pool.getRandom();
+    System.out.println("random result: " + str);
+
+    pool.delete("v");
+    System.out.println(pool);
+  }
+}
