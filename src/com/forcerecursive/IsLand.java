e51dc2d6f79a66d38f02e4d2d3c1106965548492
Second Chen <second_chen@trendmicro.com>
2021-09-08 13:22:45 +0800

    add static description

--- src/com/forcerecursive/IsLand.java
+++ src/com/forcerecursive/IsLand.java
@@ -1,2 +1,68 @@
-package com.forcerecursive;public class IsLand {
+package com.forcerecursive;
+
+/*
+ * 岛问题
+ *
+ * [题目]
+ * 一个矩阵只有0和1两中值，每个位置都可以和自己的上下左右四个位置相连，如果有一片1连在一起，这个部分叫做一个岛
+ * 求一个矩阵有多少个岛。
+ * 举例:
+ * 0 0 1 0 1 0
+ * 1 1 1 0 1 0
+ * 1 0 0 1 0 0
+ * 0 0 0 0 0 0
+ *
+ * 这个矩阵有三个岛
+ * [进阶]
+ * 如何设计一个并行算法解决这个问题
+ * ==> 并查集
+ */
+public class IsLand {
+  // 复杂度: O(M*N)
+  public static int countIsLnad(int[][] m) {
+    int M = m.length; // 行数
+    int N = m[0].length; // 列数
+    int res = 0;
+
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) {
+        if (m[i][j] == 1) {
+          res++;
+          infect(m, i, j, M, N);
+        }
+      }
+    }
+    return res;
+  }
+
+  // 将1周围所有是1元素的全部"感染", 并继续递归
+  private static void infect(int[][] m, int i, int j, int M, int N) {
+    if (i < 0 || i >= M || j < 0 || j >= N || m[i][j] != 1) {
+      return;
+    }
+    m[i][j] = 2;
+    infect(m, i - 1, j, M, N);
+    infect(m, i + 1, j, M, N);
+    infect(m, i, j - 1, M, N);
+    infect(m, i, j + 1, M, N);
+  }
+
+  public static void main(String[] args) {
+    int[][] arr = {
+      {0, 0, 0, 0},
+      {0, 1, 1, 0},
+      {0, 1, 1, 0},
+      {0, 0, 0, 0}
+    };
+
+    System.out.println("island count: " + countIsLnad(arr));
+
+    int[][] arr2 = {
+      {0, 0, 1, 0, 1, 0},
+      {1, 1, 1, 0, 1, 0},
+      {1, 0, 0, 1, 0, 0},
+      {0, 0, 0, 0, 0, 0}
+    };
+    System.out.println("island count: " + countIsLnad(arr2));
+  }
 }
--- src/com/unionset/UnionFindSet.java
+++ src/com/unionset/UnionFindSet.java
@@ -1,2 +1,102 @@
-package com.unionset;public class UnionFindSet {
+package com.unionset;
+
+import java.util.*;
+
+class Element<V> {
+  public V value;
+
+  public Element(V value) {
+    this.value = value;
+  }
+
+  public String toString() {
+    return (String) this.value;
+  }
+}
+
+public class UnionFindSet<V> {
+  public HashMap<V, Element<V>> elementMap;
+  // key 某个元素value 该元素的父
+  public HashMap<Element<V>, Element<V>> fatherMap;
+  // key 某个集合的代表元素，value 该集合大小
+  public HashMap<Element<V>, Integer> sizeMap;
+
+  public UnionFindSet(List<V> list) {
+    elementMap = new HashMap<>();
+    fatherMap = new HashMap<>();
+    sizeMap = new HashMap<>();
+    for (V value : list) {
+      Element<V> element = new Element<V>(value);
+      elementMap.put(value, element);
+      fatherMap.put(element, element); // 构造函数中每个元素都指向自己
+      sizeMap.put(element, 1);
+    }
+  }
+
+  public boolean isSameSet(V a, V b) {
+    if (elementMap.containsKey(a) || elementMap.containsKey(b)) {
+      return findHead(elementMap.get(a)) == findHead(elementMap.get(b));
+    }
+    return false;
+  }
+
+  // 找到集合的头
+  // 给定一个element, 往上一直找，把代表元素返回
+  private Element<V> findHead(Element<V> element) {
+    Stack<Element<V>> stack = new Stack<>();
+    while (element != fatherMap.get(element)) {
+      stack.push(element);
+      element = fatherMap.get(element);
+    }
+    // 到这里说明 代表元素找到，赋值为element
+    while (!stack.empty()) {
+      // 让路径上的所有元素都指向代表元素
+      fatherMap.put(stack.pop(), element);
+    }
+    return element;
+  }
+
+  public void union(V a, V b) {
+    if (elementMap.containsKey(a) || elementMap.containsKey(b)) {
+      Element<V> aF = findHead(elementMap.get(a));
+      Element<V> bF = findHead(elementMap.get(b));
+      if (aF != bF) { // else a与b在一个集合中，无须union
+        Element<V> big = sizeMap.get(aF) >= sizeMap.get(bF) ? aF : bF;
+        // small 取"短"的那个集合
+        Element<V> small = (big == aF) ? bF : aF;
+        fatherMap.put(small, big);
+        sizeMap.put(big, sizeMap.get(aF) + sizeMap.get(bF));
+        sizeMap.remove(small);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    List<String> arrayList = new ArrayList<>();
+    arrayList.add("a");
+    arrayList.add("b");
+    arrayList.add("c");
+    arrayList.add("d");
+
+    UnionFindSet<String> uSet = new UnionFindSet<>(arrayList);
+
+    System.out.println("a and b is in same set: " + uSet.isSameSet("a", "b"));
+    uSet.union("a", "b");
+    uSet.union("a", "d");
+    uSet.union("b", "c");
+    System.out.println("a and c is in same set: " + uSet.isSameSet("a", "c"));
+
+    uSet.printSet();
+  }
+
+  private void printSet() {
+    // 方法一 在for-each循环中使用entries来遍历
+    for (Map.Entry<Element<V>, Element<V>> entry : this.fatherMap.entrySet()) {
+      System.out.println(
+          "element = "
+              + entry.getKey().toString()
+              + ", father element = "
+              + entry.getValue().toString());
+    }
+  }
 }
