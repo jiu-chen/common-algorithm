ac7928642342fcc3fad151d9ba48dbc4c97f24cb
Second Chen <second_chen@trendmicro.com>
2021-11-04 12:22:32 +0800

    fix bug about 01 bag

--- src/com/forcerecursive/Knapsack.java
+++ src/com/forcerecursive/Knapsack.java
@@ -6,49 +6,54 @@ package com.forcerecursive;
  * 这个重量。返回你能装下最多的价值是多少？
  */
 public class Knapsack {
-  public static int maxValue(int[] weights, int[] values, int bag) {
-    return process(weights, values, 0, 0, bag);
-  }
 
-  // i... 的货物自由选择，形成最大价值返回
-  // 重要永远不要超过bag
-  // 之前做的决定，所达到的重量，alreadyWeight
-
-  // ! bug
-  private static int process(int[] weights, int[] values, int i, int alreadyWeight, int bag) {
-    if (alreadyWeight > bag) {
+  // n: 第一个物品 W: 背包重量
+  // weights: 物品重量数组，values 价值重量数组
+  // 倒序计算
+  public static int package1(int[] weights, int[] values, int n, int W) {
+    if (n == 0 || W == 0) // 当物品数量为0，或者背包容量为0时，最优解为0
+    {
       return 0;
     }
-    if (i == weights.length) {
-      return 0;
+    if (weights[n - 1] > W) {
+      return package1(weights, values, n - 1, W);
+    } else {
+      return Math.max(
+          package1(weights, values, n - 1, W), // 当前物品不放入袋子中，递归下一个物品
+          values[n - 1]
+              + package1(weights, values, n - 1, W - weights[n - 1]) // 当前物品放入袋子中，继续递归下一个物品
+          );
     }
-    return Math.max(
-        process(weights, values, i + 1, alreadyWeight, bag), // 当前物品不放入袋子中，递归下一个物品
-        values[i]
-            + process(
-                weights, values, i + 1, alreadyWeight + weights[i], bag) // 当前物品放入袋子中，继续递归下一个物品
-        );
   }
 
-  // dp
-  public static int maxValue2(int[] c, int[] p, int bag) {
-    int[][] dp = new int[c.length + 1][bag + 1];
-    for (int i = c.length - 1; i >= 0; i--) {
-      for (int j = bag; j >= 0; j--) {
-        dp[i][j] = dp[i + 1][j];
-        if (j + c[j] <= bag) {
-          dp[i][j] = Math.max(dp[i][j], p[i] + dp[i + 1][j + c[i]]);
+  // dp 0-1背包问题
+  // dp[i][j] 表示第i个物品装进限重为j的背包可以获得的最大价值
+  // i-1表示数组的下标
+  public static int package02(int[] weights, int[] values, int n, int W) {
+    int[][] dp = new int[n + 1][W + 1];
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= W; j++) {
+        if (j < weights[i - 1]) {
+          dp[i][j] = dp[i - 1][j];
+        } else {
+          dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i - 1]] + values[i - 1]);
         }
       }
     }
-    return dp[0][0];
+    return dp[n][W];
+  }
+
+  // todo: 01背包问题空间优化
+  // https://blog.nowcoder.net/n/c1e86561094844308cbe97cf71c88425?f=comment
+  public static int package03(int[] weights, int[] values, int n, int W) {
+    return 0;
   }
 
   public static void main(String[] args) {
     int[] weights = {1, 2, 3, 5};
-    int[] values = {8, 4, 2, 7};
+    int[] values = {2, 9, 2, 7};
     int bag = 7;
-    System.out.println("max value: " + maxValue(weights, values, bag));
-    //    System.out.println("max value: " + maxValue2(weights, values, bag));
+    System.out.println("max value: " + package1(weights, values, 4, bag));
+    System.out.println("max value: " + package02(weights, values, 4, bag)); // 15=8+7
   }
 }
